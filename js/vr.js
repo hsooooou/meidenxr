7321e7aec663d6f0e2568f1e70ffb7da0c9b3bbf
has <2sokun2580@gmail.com>
2024-03-12 07:56:41 +0900

    a

--- js/vr.js
+++ js/vr.js
@@ -2,3 +2,7 @@ $('.question').click(function(){
   $('.question').toggleClass('is-active');
   $('.vr-info').toggleClass('is-active');
 })
+
+$('.burger').click(function(){
+  $('.under-list').toggleClass('is-active');
+})
