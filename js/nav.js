3990178fb50c95b0452ac89eed840c21d4d7c142
has <2sokun2580@gmail.com>
2024-03-11 16:19:01 +0900

    ハンバーガーメニュー

--- js/nav.js
+++ js/nav.js
@@ -9,3 +9,9 @@ $(function () {
   });
 });
 
+// ハンバーガー
+// toggle あったら外すなかったら付与
+$('.burger').click(function(){
+  $('.burger').toggleClass('is-active');
+  $('.menu').toggleClass('is-active');
+})
