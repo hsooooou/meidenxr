58271beee07d5a9b965cd5fdcd01097c6ba4f789
has <2sokun2580@gmail.com>
2024-03-11 18:11:29 +0900

    a

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,3 @@
-"use strict";
-$(function () {
-  const hamburger = $(".hamburger");
-  const nav = $(".nav");
-
-  hamburger.click(function () {
-    $(this).find(".hamburger_bar").toggleClass("is_active");
-    nav.toggleClass("is_active");
-  });
-});
-
 // ハンバーガー
 // toggle あったら外すなかったら付与
 $('.burger').click(function(){
