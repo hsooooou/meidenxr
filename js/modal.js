673834990b6b0f6cb298ed679a7e4e4ecb8ae3da
has <2sokun2580@gmail.com>
2025-04-02 15:47:48 +0900

    copy

--- js/modal.js
+++ js/modal.js
@@ -21,4 +21,24 @@ $(function copyUrl() {
   navigator.clipboard.writeText(url);
 
 
+const copybtn = document.getElementById('copybtn');
+const copyok = document.getElementById('copyok');
+
+copybtn.addEventListener('click', () => {
+  // ボタンクリックでhiddenクラスを付け外しする
+  copyok.classList.toggle('copy-hidden');
+});
+
+function startTimer() {
+  timerId = setTimeout( function() {
+     copyok.classList.toggle('copy-hidden');
+  } , 5000 );
+}
+// タイマーの中止
+function abortTimer() {
+  clearTimeout( timerId );
+}
+
+setTimeout(startTimer, 2000);
+
 });
