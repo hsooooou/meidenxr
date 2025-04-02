a2746a582669d0fef0cecb6daeb0f27c4f187b9b
has <2sokun2580@gmail.com>
2025-04-02 20:30:56 +0900

    oh

--- js/modal.js
+++ js/modal.js
@@ -16,32 +16,51 @@ span.onclick = function () {
 
 
 //　リンクコピー
-$(function copyUrl() {
-  var url = 'https://forms.gle/yU6vn54oNLNisK5S8';
-  navigator.clipboard.writeText('https://forms.gle/yU6vn54oNLNisK5S8');
-  // document.execCommand('url')
+// $(function copyUrl() {
+//   var url = 'https://forms.gle/yU6vn54oNLNisK5S8';
+//   navigator.clipboard.writeText('https://forms.gle/yU6vn54oNLNisK5S8');
+//   // document.execCommand('url')
 
+//   const copybtn = document.getElementById('copybtn');
+//   const copyok = document.getElementById('copyok');
+
+//   // copybtn.addEventListener('click', () => {
+//   //   // ボタンクリックでhiddenクラスを付け外しする
+//   //   copyok.classList.toggle('copy-hidden');
+//   // });
+
+//   copybtn.addEventListener('click', () => {
+//     function startTimer() {
+//       timerId = setTimeout(function () {
+//         copyok.classList.add('copy-hidden');
+//       }, 4000);
+//     }
+//     setTimeout(startTimer, 1000);
+
+//     timerId = setTimeout(function () {
+//       copyok.classList.remove('copy-hidden');
+//     }, 500);
+//   });
+//   navigator.clipboard.writeText('https://forms.gle/yU6vn54oNLNisK5S8');
+//   return
+// });
+
+$(function () {
   const copybtn = document.getElementById('copybtn');
   const copyok = document.getElementById('copyok');
-
-  // copybtn.addEventListener('click', () => {
-  //   // ボタンクリックでhiddenクラスを付け外しする
-  //   copyok.classList.toggle('copy-hidden');
-  // });
+  const url = 'https://forms.gle/yU6vn54oNLNisK5S8';
 
   copybtn.addEventListener('click', () => {
-    function startTimer() {
-      timerId = setTimeout(function () {
+    navigator.clipboard.writeText(url).then(() => {
+      // コピー成功時の処理
+      copyok.classList.remove('copy-hidden');
+
+      // 4秒後に非表示にする
+      setTimeout(() => {
         copyok.classList.add('copy-hidden');
       }, 4000);
-    }
-    setTimeout(startTimer, 1000);
-
-    timerId = setTimeout(function () {
-      copyok.classList.remove('copy-hidden');
-    }, 500);
+    }).catch(err => {
+      console.error('コピーに失敗しました', err);
+    });
   });
-  navigator.clipboard.writeText('https://forms.gle/yU6vn54oNLNisK5S8');
-  return
 });
-
