2c5e510fc401390ba7d311f5ce0cd64cc00f6a71
has <2sokun2580@gmail.com>
2025-05-08 22:47:25 +0900

    ar-form

---
+++ js/ar.js
@@ -0,0 +1,27 @@
+// サポート画面開く
+$('.question').click(function(){
+  $('.question').toggleClass('is-active');
+  $('.vr-info').toggleClass('is-active');
+})
+
+$('.burger').click(function(){
+  $('.under-list').toggleClass('is-active');
+})
+
+// 合言葉モーダル
+function checkKeyword() {
+  const correctKeyword = "とまと";  // 正解の合言葉をここに設定
+  const input = document.getElementById("keywordInput").value.trim();
+  const result = document.getElementById("keywordResult");
+
+  if (input === correctKeyword) {
+    window.location.href = "./ar-finish.html";
+  } else {
+    result.innerHTML = "合言葉が違うよ！<br>もう一度試してね。";
+    result.style.display = "block";
+
+    setTimeout(() => {
+      result.style.display = "none";
+    }, 10000);
+  }
+}
