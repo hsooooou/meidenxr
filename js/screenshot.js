a4a445aca4f61f170f129066a30cb8b259b232dd
has <2sokun2580@gmail.com>
2025-05-08 17:13:49 +0900

    ar

--- js/screenshot.js
+++ js/screenshot.js
@@ -7,6 +7,7 @@ document.getElementById('screenshot').addEventListener('click', function () {
     console.warn("Canvas or video not ready");
     return;
   }
+  
 
   const now = new Date();
   const fileName = `ar_screenshot_${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}.png`;
