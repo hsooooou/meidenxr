e8f62770aa679c5a1556d95a1b421cb84e767a06
has <2sokun2580@gmail.com>
2025-08-11 17:30:05 +0900

    ar

--- js/ar.js
+++ js/ar.js
@@ -90,7 +90,7 @@ window.addEventListener('DOMContentLoaded', () => { //全て読み込まれて�
     },
     {
       //n08-sc サテライト教室
-      patternUrl: 'pattern-testplay_1_n08-sc.patt',
+      patternUrl: 'pattern-testplay_1_n08-sc',
       link: 'https://hsooooou.github.io/meidenxr/schoolmap/vr-n08-sc.html',
       buttonId: 'button-n08-sc'
     },
